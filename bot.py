52b21952f7786932fa1d344b36b571043c1c48af
ItsBubbles <72664664+ItsBubbles@users.noreply.github.com>
2021-02-25 18:20:26 -0500

    Added roullete game

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import discord
 import time
 import random
 import json
+from discord import Webhook
 from discord.ext import commands
 from discord.ext.commands import bot, BucketType, CommandOnCooldown, errors
 import scrape
@@ -13,6 +14,46 @@ from scrape import discordjokesingle
 client = commands.Bot(command_prefix = "!")
 bot = commands.Bot(command_prefix='?')
 dealercardrandom = 0
+
+roulletepictures = [
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531285312602122/Ballon0.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531287955013642/Ballon1.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531292170944512/Ballon2.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531299594731570/Ballon3.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531303406960640/Ballon4.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531306753884212/Ballon5.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531310777139260/Ballon6.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531315986333736/Ballon7.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531320956583986/Ballon8.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531325230186536/Ballon9.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531329818624008/Ballon10.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531333073666058/Ballon11.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531337405857832/Ballon12.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531341244694558/Ballon13.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531344004677642/Ballon14.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531347698810880/Ballon15.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531350358786098/Ballon16.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531352551882772/Ballon17.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531355429044304/Ballon18.png",
+    "https://cdn.discordapp.com/attachments/593265613527580674/814531356994437130/Ballon19.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532465057333288/Ballon20.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532475321581628/Ballon21.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532531248562247/Ballon22.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532541876011018/Ballon23.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532549120098354/Ballon24.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532557257179226/Ballon25.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532565746581504/Ballon26.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532574687920188/Ballon27.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532581541937152/Ballon28.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532622889910352/Ballon29.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532628253638686/Ballon30.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532635304394752/Ballon31.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532641969012797/Ballon32.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532647971586058/Ballon33.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532656250355712/Ballon34.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532662197878784/Ballon35.png",
+    "https://cdn.discordapp.com/attachments/666082097714167808/814532693038596126/Ballon36.png"
+]
 
 
 @client.event
@@ -55,7 +96,7 @@ async def coinflip(ctx):
 
     coinflipstring= str(coinflipwager.content)
     coinflipfloat = float(coinflipstring)
-    coinflipmath = coinflipfloat * 0.5
+    coinflipmath = coinflipfloat * 2
     coinflipwinnings = int(coinflipmath)
 
     while (working == True):
@@ -64,14 +105,14 @@ async def coinflip(ctx):
             working = False
             break
             
-        if str(coinflipresponse.content) == "Heads":
+        if str(coinflipresponse.content.lower()) == "heads":
             if randomcoin == 0:
                 users[str(user.id)]["bank"] += coinflipwinnings
                 with open("mainbank.json", "w") as f:
                     json.dump(users, f)
 
                 finalem = discord.Embed(title = "CoinFlip Results", color = discord.Color.purple())
-                finalem.add_field(name = "Heads", value = f"You won {coinflipwinnings} $")
+                finalem.add_field(name = "Heads", value = f"You won {coinflipfloat} $")
                 await ctx.send (embed = finalem)
                 working = False
 
@@ -86,7 +127,7 @@ async def coinflip(ctx):
                 await ctx.send (embed = finalem)
                 working = False
         
-        elif str(coinflipresponse.content) == "Tails":
+        elif str(coinflipresponse.content.lower) == "tails":
             if randomcoin == 1:
 
                 users[str(user.id)]["bank"] += coinflipwinnings
@@ -94,7 +135,7 @@ async def coinflip(ctx):
                     json.dump(users, f)
                 
                 finalem = discord.Embed(title = "CoinFlip Results", color = discord.Color.purple())
-                finalem.add_field(name = "Tails", value = f"You won {coinflipwinnings} $")
+                finalem.add_field(name = "Tails", value = f"You won {coinflipfloat} $")
                 await ctx.send (embed = finalem)
                 working = False
             else:
@@ -113,6 +154,7 @@ async def coinflip(ctx):
 @client.command()
 @commands.cooldown(1,5)
 async def blackjack(ctx):
+    
     global dealercardrandom
     usercard1 = random.randint(1,11)
     usercard2 = random.randint(1,11)
@@ -121,13 +163,14 @@ async def blackjack(ctx):
     usercard5 = random.randint(1,11)
     usercard6 = random.randint(1,11)
     dealercardrandom = random.randint(1,11)
+
     Working = True
 
     await open_account(ctx.author)
     user = ctx.author
     users = await get_bank_data()
     bankamount = users[str(user.id)]["bank"]
-
+    
     await ctx.send("What is your wager")
     
     def is_correct(m):
@@ -139,21 +182,21 @@ async def blackjack(ctx):
     userwagerint = int(userwagerread)
     userwagerwinnings = userwagerint
     userwagerlosings = userwagerint * -1
-    
+
     while (Working == True):
-        if(userwagerint> bankamount):
+        if(userwagerint > bankamount):
             await ctx.send("You are to broke, try again")
             Working = False
             break
 
         startem = discord.Embed(title = f"{ctx.author.name}'s Cards", color = discord.Color.purple())
-        startem.add_field(name = f"Your Cards ", value = f"{usercard1}  {usercard2}")
+        startem.add_field(name = f"Your Cards ", value = f"{usercard1} {usercard2}")
         startem.add_field(name = f"Dealer Cards", value = f"{dealercardrandom}", inline = False)
         await ctx.send (embed = startem)
 
         userhit = await client.wait_for("message", check = is_correct)
 
-        if str(userhit.content) == "Hit":
+        if str(userhit.content.lower()) == "hit":
             playercardtotal = usercard1 + usercard2 + usercard3
         
             if (playercardtotal > 21):
@@ -173,11 +216,10 @@ async def blackjack(ctx):
                 await ctx.send (embed = hitem)
                 working = True
 
-        if str(userhit.content) == "Stand":
+        if str(userhit.content.lower()) == "stand":
 
             dealercardtotal1 = getnumber17(dealercardrandom)
-            print(dealercardtotal1)
-
+        
             playercardtotal = usercard1 + usercard2
             
             if (dealercardtotal1 <= 21 and dealercardtotal1 >= 17 and dealercardtotal1 < playercardtotal):
@@ -218,7 +260,7 @@ async def blackjack(ctx):
             
         userhit2 = await client.wait_for("message", check = is_correct)
 
-        if str(userhit2.content) == "Hit":
+        if str(userhit2.content.lower()) == "hit":
 
             playercardtotal = usercard1 + usercard2 + usercard3 + usercard4
 
@@ -238,10 +280,9 @@ async def blackjack(ctx):
                 hitem2.add_field(name = f"Dealer Cards", value = f"{dealercardrandom}", inline = False)
                 await ctx.send (embed = hitem2)
                 Working = True
-        if str(userhit2.content) == "Stand":
+        if str(userhit2.content.lower()) == "stand":
             dealercardtotal1 = getnumber17(dealercardrandom)
-            print(dealercardtotal1)
-
+            
             playercardtotal = usercard1 + usercard2 + usercard3
             
             if (dealercardtotal1 <= 21 and dealercardtotal1 >= 17 and dealercardtotal1 < playercardtotal):
@@ -282,7 +323,7 @@ async def blackjack(ctx):
         
         userhit3 = await client.wait_for("message", check = is_correct)
 
-        if str(userhit3.content) == "Hit":
+        if str(userhit3.content.lower()) == "hit":
 
             playercardtotal = usercard1 + usercard2 + usercard3 + usercard4 + usercard5
         
@@ -302,9 +343,9 @@ async def blackjack(ctx):
                 await ctx.send (embed = hitem3)
                 Working = True
         
-            if str(userhit3.content) == "Stand":
+            if str(userhit3.content.lower()) == "stand":
                 dealercardtotal1 = getnumber17(dealercardrandom)
-                print(dealercardtotal1)
+                
 
                 playercardtotal = usercard1 + usercard2 + usercard3 + usercard4
                 
@@ -344,7 +385,7 @@ async def blackjack(ctx):
         
 
             userhit4 = await client.wait_for("message", check = is_correct)
-            if str(userhit4.content) == "Hit":
+            if str(userhit4.content.lower()) == "hit":
                 playercardtotal = usercard1 + usercard2 + usercard3 + usercard4 + usercard5 + usercard6
 
                 if (usercard1 + usercard2 + usercard3 + usercard4 + usercard5  + usercard6> 21):
@@ -364,9 +405,9 @@ async def blackjack(ctx):
                     Working = True
 
             
-            if str(userhit4.content) == "Stand":
+            if str(userhit4.content.lower()) == "stand":
                 dealercardtotal1 = getnumber17(dealercardrandom)
-                print(dealercardtotal1)
+                
 
                 playercardtotal = usercard1 + usercard2 +usercard3 + usercard4 + usercard5
                 
@@ -403,7 +444,6 @@ async def blackjack(ctx):
                         json.dump(users, f)
                     await ctx.send (embed = standem2)
                     Working = False
-
 
 def getnumber17(dealercardrandom):
     
@@ -470,6 +510,115 @@ async def getmoney(ctx):
 
     with open("mainbank.json", "w") as f:
         json.dump(users, f)
+
+
+@client.command()
+async def roullete(ctx):
+    Working = True
+    rednumbers = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+    blacknumbers = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+    roulletenum = random.randint(0,36)
+
+    await open_account(ctx.author)
+    user = ctx.author
+    users = await get_bank_data()
+    bankamount = users[str(user.id)]["bank"]
+
+    await ctx.send("What is your pick and wager")
+
+    def is_correct(m):
+        return m.author == ctx.author 
+    
+    userresponse = await client.wait_for("message", check = is_correct)
+    userstring = str(userresponse.content)
+    userchoice = userstring.split()
+    userguess = userchoice[0]
+    userwager = int(userchoice[1])
+    userwagerwinnings = userwager * 2
+    userwagerlosing = userwager * -1
+    
+    while(Working == True):
+        
+        if(userwager > bankamount):
+                await ctx.send("You are to broke, try again")
+                Working = False
+                break
+        
+        if userguess.lower() == "red":
+            
+            if roulletenum in rednumbers:
+                redem = discord.Embed(title = f"{ctx.author.name} You Win")
+                redem.add_field(name = f"Winnings", value = f"{userwager * 2} $")
+                redem.add_field(name = f"Your Pick", value = f"{userguess}")
+                redem.set_image(url = roulletepictures[roulletenum])              
+                await ctx.send(embed = redem)
+                users[str(user.id)]["bank"] += userwagerwinnings
+                with open("mainbank.json", "w") as f:
+                    json.dump(users, f)
+                Working = False
+            else:
+                redem = discord.Embed(title = f"{ctx.author.name} You Lose")
+                redem.add_field(name = f"Losings", value = f"{userwager} $")
+                redem.add_field(name = f"Your Pick", value = f"{userguess}")
+                redem.set_image(url = roulletepictures[roulletenum])
+                await ctx.send(embed = redem)
+                users[str(user.id)]["bank"] += userwagerlosing
+                with open("mainbank.json", "w") as f:
+                    json.dump(users, f)
+                Working = False
+        if userguess.lower() == "black":
+            
+            if roulletenum in blacknumbers:
+                redem = discord.Embed(title = f"{ctx.author.name} You Win")
+                redem.set_image(url = roulletepictures[roulletenum])
+                redem.add_field(name = f"Winnings", value = f"{userwager * 2} $")
+                redem.add_field(name = f"Your Pick", value = f"{userguess}")
+                await ctx.send(embed = redem)
+                users[str(user.id)]["bank"] += userwagerwinnings
+                with open("mainbank.json", "w") as f:
+                    json.dump(users, f)
+                Working = False
+                
+            else:
+                redem = discord.Embed(title = f"{ctx.author.name} You Lose")
+                redem.add_field(name = f"Losings", value = f"{userwager} $")
+                redem.add_field(name = f"Your Pick", value = f"{userguess}")
+                redem.set_image(url = roulletepictures[roulletenum])
+                await ctx.send(embed = redem)
+                users[str(user.id)]["bank"] += userwagerlosing
+                with open("mainbank.json", "w") as f:
+                    json.dump(users, f)
+                Working = False
+        if int(userguess) in range(0, 36):
+
+            if int(userguess) == roulletenum:
+                redem = discord.Embed(title = f"{ctx.author.name} You Win")
+                redem.set_image(url = roulletepictures[roulletenum])
+                redem.add_field(name = f"Winnings", value = f"{userwager * 36} $")
+                redem.add_field(name = f"Your Pick", value = f"{userguess}")
+                await ctx.send(embed = redem)
+                users[str(user.id)]["bank"] += userwager * 36
+                with open("mainbank.json", "w") as f:
+                    json.dump(users, f)
+                Working = False
+                
+            else:
+                redem = discord.Embed(title = f"{ctx.author.name} You Lose")
+                redem.add_field(name = f"Losings", value = f"{userwager} $")
+                redem.add_field(name = f"Your Pick", value = f"{userguess}")
+                redem.set_image(url = roulletepictures[roulletenum])
+                await ctx.send(embed = redem)
+                users[str(user.id)]["bank"] += userwagerlosing
+                with open("mainbank.json", "w") as f:
+                    json.dump(users, f)
+                Working = False
+
+
+
+
+
+  
+
 
 @client.event
 async def on_command_error(ctx, error):
