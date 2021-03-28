1b2f211c34a8f006da7c9ff217dab1cce096ca50
ItsBubbles <seattlerocks11@gmail.com>
2021-03-28 18:27:58 -0400

    Added Constructors Championship winners

--- f1.py
+++ f1.py
@@ -15,7 +15,7 @@ def raceSchedule(raceNumber):
 
     return(schedulejson["raceName"], raceLocation["country"], schedulejson["date"], circuitName["circuitName"], raceLocation["locality"])
 
-print(raceSchedule(0))
+print(raceSchedule(5))
 
 schedule = requests.get("http://ergast.com/api/f1/2021.json")
 schedulejson = json.loads(schedule.text)["MRData"]["RaceTable"]["Races"][1]["Circuit"]
